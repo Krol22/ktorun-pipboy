8194974c8085decaa6dc9c55f4e94edbab6af573
Karol Torun <krol22ee@gmail.com>
2020-05-12 19:04:38 +0200

    Changing description to text with mods and weight and condition, added descriptions and weight

--- src/main/uses-component.js
+++ src/main/uses-component.js
@@ -13,33 +13,87 @@ export class UsesComponent extends HTMLElement {
                 <div class="uses__items" data-item="items">
                     <div class="uses__item" data-item="terminal">
                         <div class="uses__icon">TestIcon</div> 
-                        <div class="uses__text u-right-top-to-bottom">
-                            <a class="link" href="https://iterm2.com" target="blank">Iterm2</a> - 
+                        <div class="uses__description">
+                            <div class="uses__condition u-right-top-to-bottom">
+                                <div class="uses__condition--text">CND</div>
+                                <div class="uses__condition--value">--</div>
+                            </div>
+                            <div class="uses__weight u-right-top-to-bottom">
+                                <div class="uses__weight--text">WG</div>
+                                <div class="uses__weight--value">37.1MB</div>
+                            </div>
+                            <div class="uses__text u-right-top-to-bottom">
+                                MODS default shell ZSH,<br />
+                            </div>
                         </div>
                     </div>
                     <div class="uses__item" data-item="editor">
                         <div class="uses__icon">TestIcon</div> 
-                        <div class="uses__text u-right-top-to-bottom">
-                            <a class="link" href="https://neovim.io/" target="blank">Neovim</a> -
-                            <a class="link" href="https://github.com/Krol22/yadf/tree/master/nvim" target="blank">My configuration</a>
+                        <div class="uses__description">
+                            <div class="uses__condition u-right-top-to-bottom">
+                                <div class="uses__condition--text">CND</div>
+                                <div class="uses__condition--value">--</div>
+                            </div>
+                            <div class="uses__weight u-right-top-to-bottom">
+                                <div class="uses__weight--text">WG</div>
+                                <div class="uses__weight--value">8.97MB</div>
+                            </div>
+                            <div class="uses__text u-right-top-to-bottom">
+                                LOOK LIKE HACKER +9<br />
+                                MODS - TOO MANY, check <a class="link" href="https://github.com/Krol22/yadf/tree/master/nvim" target="blank">here</a>.
+                            </div>
                         </div>
                     </div>
                     <div class="uses__item" data-item="macbook">
                         <div class="uses__icon">TestIcon</div> 
-                        <div class="uses__text u-right-top-to-bottom">
-                            <a class="link" href="https://support.apple.com/kb/SP719" target="blank">Macbook</a> - 
+                        <div class="uses__description">
+                            <div class="uses__condition u-right-top-to-bottom">
+                                <div class="uses__condition--text">CND</div>
+                                <div class="uses__condition--value">--</div>
+                            </div>
+                            <div class="uses__weight u-right-top-to-bottom">
+                                <div class="uses__weight--text">WG</div>
+                                <div class="uses__weight--value">2.05KG</div>
+                            </div>
+                            <div class="uses__text u-right-top-to-bottom">
+                                BUILDING COOL STUFF SPEED +7, <br />
+                                MODS: bunch of stickers,
+                            </div>
                         </div>
                     </div>
                     <div class="uses__item" data-item="keyboard">
                         <div class="uses__icon">TestIcon</div> 
-                        <div class="uses__text u-right-top-to-bottom">
-                            In my home office, I use a 20 year old keyboard (DIN 5) which I recently found in the attic. I don't know the model exactly, but it looks like <a class="link" href="https://www.cnet.com/products/btc-5121-keyboard/" target="blank">this</a>.
+                        <div class="uses__description">
+                            <div class="uses__condition u-right-top-to-bottom">
+                                <div class="uses__condition--text">CND</div>
+                                <div class="uses__condition--value">--</div>
+                            </div>
+                            <div class="uses__weight u-right-top-to-bottom">
+                                <div class="uses__weight--text">WG</div>
+                                <div class="uses__weight--value">487 G</div>
+                            </div>
+                            <div class="uses__text u-right-top-to-bottom">
+                                SPEED +2,<br />
+                                MODS PS2 to USB adapter,<br />
+                                MODS DIN5 to PS2 converter,<br />
+                            </div>
                         </div>
                     </div>
                     <div class="uses__item" data-item="monitor">
                         <div class="uses__icon">TestIcon</div> 
-                        <div class="uses__text u-right-top-to-bottom">
-                            <a class="link" href="https://www.lg.com/us/monitors/lg-27UD69P-W-4k-uhd-led-monitor" target="blank">LG 27"</a> - 
+                        <div class="uses__description">
+                            <div class="uses__condition u-right-top-to-bottom">
+                                <div class="uses__condition--text">CND</div>
+                                <div class="uses__condition--value">--</div>
+                            </div>
+                            <div class="uses__weight u-right-top-to-bottom">
+                                <div class="uses__weight--text">WG</div>
+                                <div class="uses__weight--value">5.2KG</div>
+                            </div>
+                            <div class="uses__text u-right-top-to-bottom">
+                                CODING SPACES +1, <br />
+                                EFFECTS: PIVOT,
+                            </div>
                         </div>
                     </div>
                 </div>
